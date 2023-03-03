8577f3e65a269045d4b57afcc0b59e79eaa8eb60
Emanuel Jaime <emanuel.jaime@solera.com>
2023-03-03 10:19:59 +0100

    Fixed Hardcode values

--- src/test/java/com/google/behavior/Actions.java
+++ src/test/java/com/google/behavior/Actions.java
@@ -1,29 +1,38 @@
 package com.google.behavior;
 
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.google.utils.LoadProperties;
+
 public class Actions {
+	
+	private static Properties properties = LoadProperties.getProperties();
 	
 	public static void goToPage(WebDriver driver) {
 		driver.manage().window().maximize();
-		String url = "https://www.google.com/";
+		String url = properties.getProperty("googleUrl");
 		driver.get(url);
 	}
 	
 	public static void acceptCookies(WebDriver driver) {
-		WebElement acceptCookiesBtn = driver.findElement(By.id("L2AGLb"));
+		String idBtn = properties.getProperty("AcceptCookiesBtnId");
+		WebElement acceptCookiesBtn = driver.findElement(By.id(idBtn));
 		acceptCookiesBtn.click();
 	}
 	
 	public static void searchBar (WebDriver driver, String item) {
-		WebElement searchBar = driver.findElement(By.className("gLFyf"));
+		String searchClass = properties.getProperty("searchClassName");
+		WebElement searchBar = driver.findElement(By.className(searchClass));
 		searchBar.sendKeys(item);
 	}
 	
 	public static void searchBtn (WebDriver driver) {
-		WebElement searchBtn = driver.findElement(By.className("gNO89b"));
+		String searchBtnClass = properties.getProperty("searchBtnClassName");
+		WebElement searchBtn = driver.findElement(By.className(searchBtnClass));
 		searchBtn.click();
 	}
 	
